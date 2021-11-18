0543514b7c3d1197f8baa5ef847320239b2580f4
Gustavo Almeida <73246160+gustavoalmei@users.noreply.github.com>
2021-11-17 22:30:05 -0300

    add background e new functions

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ let backMusic = document.querySelector('#backMusic')
 let songs = document.querySelector('#songs')
 let author = document.querySelector('#author')
 let nameMusic = document.querySelector('#nameMusic')
+let photoAlbum = document.querySelector('#photoAlbum')
 
 // Convertendo time do audio, e configurando time live
 function convertTimeAudio() {
@@ -42,40 +43,44 @@ function convertTimeAudio() {
 // Definindo tempo de intervalo para executar a função
 setInterval(convertTimeAudio, 1000)
 
-function timeMusic(){
+function timeMusic() {
   audioPlayer.currentTime = progress.value
 }
 
 // Músicas
-
-const track = [{
+const track = [
+  {
     index: 0,
-    nome:"Matue",
-    musica: "Anos luz",
-    src: "assets/music/Matuê_Anos_Luz.mp4"
+    nome: 'Matue',
+    musica: 'Anos luz',
+    src: 'assets/music/Matuê_Anos_Luz.mp4',
+    photo: 'assets/img/matue_anos_luz.png'
   },
 
   {
     index: 1,
-    nome:"Lagum",
-    musica: "EU E MINHAS PARANOIAS",
-    src: "assets/music/Lagum_EU_E_MINHAS_PARANOIAS.mp3"
+    nome: 'Lagum',
+    musica: 'EU E MINHAS PARANOIAS',
+    src: 'assets/music/Lagum_EU_E_MINHAS_PARANOIAS.mp3',
+    photo: 'assets/img/Lagum.png'
   },
 
   {
     index: 2,
-    nome:"Lagum",
-    musica: "Ninguém Me Ensinou (KVSH Remix )",
-    src: "assets/music/Lagum_Ninguem_Me_Ensinou.mp3"
+    nome: 'Lagum',
+    musica: 'Ninguém Me Ensinou (KVSH Remix )',
+    src: 'assets/music/Lagum_Ninguem_Me_Ensinou.mp3',
+    photo: 'assets/img/Lagum.png'
   },
 
   {
     index: 3,
-    nome:"Jovem Dionisio - Pontos de Exclamação (Vintage Culture & Future Class Remix)",
-    musica: "Pontos de exclamações",
-    src: "assets/music/Jovem_Dionisio_Pontos_de_Exclamação.mp3"
-}
-];
+    nome: 'Jovem Dionisio - Pontos de Exclamação (Vintage Culture & Future Class Remix)',
+    musica: 'Pontos de exclamações',
+    src: 'assets/music/Jovem_Dionisio_Pontos_de_Exclamação.mp3',
+    photo: 'assets/img/JovemDionisio.png'
+  }
+]
 
 // Configurando barra de progresso da música
 function ProgressAudio() {
@@ -83,48 +88,51 @@ function ProgressAudio() {
   progress.value = audioPlayer.currentTime
 }
 // Definindo tempo de intervalo para executar a função
-setInterval(ProgressAudio,100)
-
+setInterval(ProgressAudio, 100)
 
 //Play Pause Musica
-
-let playing = true;
-function PlayMusic(){
-  
-  if(playing){
-    playPause.setAttribute("src", "./assets/img/pause-circle-regular-24.png")
+let playing = true
+function PlayMusic() {
+  if (playing) {
+    playPause.setAttribute('src', './assets/img/pause-circle-regular-50.png')
     audioPlayer.play()
-    return playing = false
-  }else{
-    playPause.setAttribute("src", "./assets/img/play-circle-regular-24.png")
+    return (playing = false)
+  } else {
+    playPause.setAttribute('src', './assets/img/play-circle-regular-50.png')
     audioPlayer.pause()
-    return playing = true
+    return (playing = true)
   }
 }
 
 //Avançar 10s
-function MoreSecondMusic(){
+function MoreSecondMusic() {
   audioPlayer.currentTime += 10
 }
 
 //Voltar 10s
-function LessSecondMusic(){
+function LessSecondMusic() {
   audioPlayer.currentTime -= 10
 }
 
 let soundMuted = false
 
 // controle de audio e som
-function ChangeVolume(){
+function ChangeVolume() {
   audioPlayer.volume = InputVolume.value / 100
 
-  if(InputVolume.value == 0){
-    sound.setAttribute("src", "./assets/img/volume-mute-regular-24.png")
+  if (InputVolume.value == 0) {
+    sound.setAttribute('src', './assets/img/volume-mute-regular-50.png')
     audioPlayer.muted = true
     soundMuted = true
     return soundMuted
-  }else{
-    sound.setAttribute("src", "./assets/img/volume-full-regular-24.png")
+  } 
+  else if(InputVolume.value <= 30) {
+    sound.setAttribute('src', './assets/img/volume-low-regular-48.png')
+    audioPlayer.muted = false
+    soundMuted = false
+    return soundMuted
+  }else {
+    sound.setAttribute('src', './assets/img/volume-full-regular-50.png')
     audioPlayer.muted = false
     soundMuted = false
     return soundMuted
@@ -132,58 +140,70 @@ function ChangeVolume(){
 }
 
 // Mutando
-let UserVol;
-function mute(){
+let UserVol
+function mute() {
   UserVol = audioPlayer.volume * 100
 
-  if(audioPlayer.muted == true){
-    sound.setAttribute("src", "./assets/img/volume-full-regular-24.png")
+  if (audioPlayer.muted == true) {
+    sound.setAttribute('src', './assets/img/volume-full-regular-50.png')
     audioPlayer.muted = false
     InputVolume.value = UserVol
-  }else{
-    sound.setAttribute("src", "./assets/img/volume-mute-regular-24.png")
+  } else {
+    sound.setAttribute('src', './assets/img/volume-mute-regular-50.png')
     audioPlayer.muted = true
     InputVolume.value = 0
   }
 }
 
 //Mudando música, avançando e voltando
-let songIndex = 0;
+let songIndex = 0
 
-function nextMusic(){
-  songIndex++;
+function nextMusic() {
+  songIndex++
 
-  if(songIndex > track.length){  
-    alert("Limite máximo de músicas.")
-  }else{
-    songs.src = track[songIndex].src;
-    author.innerText = track[songIndex].nome;
-    nameMusic.innerText = track[songIndex].musica;
-
-    audioPlayer.load()
-    PlayMusic()
-  }
-}
-
-function backtMusic(){
-  songIndex--;
-
-  if(songIndex < 0){  
-    alert("Limite máximo de músicas.")
-  }else{
-    songs.src = track[songIndex].src;
-    author.innerText = track[songIndex].nome;
-    nameMusic.innerText = track[songIndex].musica;
+  if (songIndex >= track.length) {
+    alert('Limite máximo de músicas.')
+  } else {
+    songs.src = track[songIndex].src
+    author.innerText = track[songIndex].nome
+    nameMusic.innerText = track[songIndex].musica
+    photoAlbum.src = track[songIndex].photo
 
     audioPlayer.load()
     PlayMusic()
   }
 }
 
-playPause.addEventListener("click", PlayMusic)
-advTime.addEventListener("click", MoreSecondMusic)
-backTime.addEventListener("click", LessSecondMusic)
-InputVolume.addEventListener("input", ChangeVolume)
-progress.addEventListener("input", timeMusic)
-NextMusic.addEventListener("click", nextMusic)
-backMusic.addEventListener("click", backtMusic)
+function backtMusic() {
+  songIndex--
+
+  if (songIndex < 0) {
+    alert('Limite máximo de músicas.')
+  } else {
+    songs.src = track[songIndex].src
+    author.innerText = track[songIndex].nome
+    nameMusic.innerText = track[songIndex].musica
+    photoAlbum.src = track[songIndex].photo
+
+    audioPlayer.load()
+    PlayMusic()
+  }
+}
+
+// Definidno função para carregar a primeira faixa com a página, e definir volume padrão
+window.onload = () => {
+  songs.src = track[songIndex].src
+  author.innerText = track[songIndex].nome
+  nameMusic.innerText = track[songIndex].musica
+  photoAlbum.src = track[songIndex].photo
+
+  audioPlayer.volume = 0.5
+}
+
+playPause.addEventListener('click', PlayMusic)
+advTime.addEventListener('click', MoreSecondMusic)
+backTime.addEventListener('click', LessSecondMusic)
+InputVolume.addEventListener('input', ChangeVolume)
+progress.addEventListener('input', timeMusic)
+NextMusic.addEventListener('click', nextMusic)
+backMusic.addEventListener('click', backtMusic)
